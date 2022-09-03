c92d07e7ba68e8f40bbde85d3b8f72dc39a7c7b1
coffee-r <game.developer.ryo@gmail.com>
2022-09-03 11:14:42 +0900

    スマホのhover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,21 @@
+// スマホでhoverが残り続けてしまう問題を回避するためのフラグ員
+const plugin = require('tailwindcss/plugin')
+const hoverPlugin = plugin(({ addVariant, e, postcss }) => {
+  addVariant('hover', ({ container, separator }) => {
+    const hoverRule = postcss.atRule({
+      name: 'media',
+      params: '(hover: hover)',
+    })
+    hoverRule.append(container.nodes)
+    container.append(hoverRule)
+    hoverRule.walkRules((rule) => {
+      rule.selector = `.${e(
+        `hover${separator}${rule.selector.slice(1)}`
+      )}:hover`
+    })
+  })
+})
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -8,9 +26,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
+    hoverPlugin,
     require('@tailwindcss/typography'),
   ],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
 }
